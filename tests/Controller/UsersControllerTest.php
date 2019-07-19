3082c28f96dddb43f52cf6e4c86f55a524fc2632
Daniele Megna <megna.dany@gmail.com>
2019-07-19 18:44:56 +0200

    Improve UsersController e2e test

--- tests/Controller/UsersControllerTest.php
+++ tests/Controller/UsersControllerTest.php
@@ -14,28 +14,14 @@ class UsersControllerTest extends WebTestCase {
 
   public function testScenario() {
     $this->assertEquals([], $this->retrieveUsers());
-
-    $shadyId = $this->registerUser([
-      'username' => 'shady90',
-      'password' => 'very$ecure',
-      'about' => 'About shady90 here.',
-    ]);
-    $mariaId = $this->registerUser([
-      'username' => 'maria89',
-      'password' => 'yeah$ecure',
-      'about' => 'About maria89 here.',
-    ]);
+    $shadyId = $this->registerUser('shady90', 'About shady90 here.', 'very$ecure');
+    $mariaId = $this->registerUser('maria89','About maria89 here.', 'yeah$ecure');
 
     $actual = $this->retrieveUsers();
-    $expected = [[
-      'id' => $shadyId,
-      'username' => 'shady90',
-      'about' => 'About shady90 here.',
-    ], [
-      'id' => $mariaId,
-    'username' => 'maria89',
-    'about' => 'About maria89 here.',
-    ]];
+    $expected = [
+      ['id' => $shadyId, 'username' => 'shady90', 'about' => 'About shady90 here.'],
+      ['id' => $mariaId, 'username' => 'maria89', 'about' => 'About maria89 here.']
+    ];
     $this->assertArrayContainsExactlyInAnyOrder($expected, $actual);
   }
 
@@ -48,16 +34,20 @@ class UsersControllerTest extends WebTestCase {
     return json_decode($response->getContent(), true);
   }
 
-  private function registerUser($user) {
-    $this->postAsJson($user);
+  private function registerUser($username, $about, $password) {
+    $this->postAsJson([
+      'username' => $username,
+      'password' => $password,
+      'about' => $about
+    ]);
 
     $response = $this->client->getResponse();
     $this->assertEquals(200, $response->getStatusCode());
     $this->assertEquals("application/json", $response->headers->get("content-type"));
     $responseBody = json_decode($response->getContent(), true);
     $this->assertTrue(array_key_exists("id", $responseBody));
-    $this->assertEquals($user["username"], $responseBody["username"]);
-    $this->assertEquals($user["about"], $responseBody["about"]);
+    $this->assertEquals($username, $responseBody["username"]);
+    $this->assertEquals($about, $responseBody["about"]);
 
     return $responseBody["id"];
   }
