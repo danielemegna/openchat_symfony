e1027f7e9b59234fa61a2841381c3c3e5379ee49
Daniele Megna <megna.dany@gmail.com>
2019-07-20 17:17:02 +0200

    Refactoring in UsersController

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -16,34 +16,45 @@ class UsersController extends Controller {
    * @Route("/users", methods={"GET"})
    */
   public function retrieveUsers(RetrieveUsersUseCase $usecase) {
-    $responseBody = array_map(function($u) {
-      return [
-        'id' => $u->getId(),
-        'username' => $u->getUsername(),
-        'about' => $u->getAbout()
-      ];
-    }, $usecase->run());
-
-    return $this->json($responseBody);
+    $users = $usecase->run();
+    return $this->serializeUsers($users);
   }
 
   /**
    * @Route("/users", methods={"POST"})
    */
   public function registerUser(Request $request, RegisterUserUseCase $usecase) {
-    $requestBody = json_decode($request->getContent());
-    $userToBeRegistered = User::newWithoutId(
-      $requestBody->username,
-      $requestBody->about,
-      $requestBody->password
-    );
-
+    $userToBeRegistered = $this->deserializeUser($request->getContent());
     $createdUser = $usecase->run($userToBeRegistered);
+    return $this->serializeUser($createdUser);
+  }
 
+  private function serializeUsers($users) {
+    $responseBody = array_map(function($u) {
+      return [
+        'id' => $u->getId(),
+        'username' => $u->getUsername(),
+        'about' => $u->getAbout()
+      ];
+    }, $users);
+
+    return $this->json($responseBody);
+  }
+
+  private function deserializeUser($jsonString) {
+    $json = json_decode($jsonString);
+    return User::newWithoutId(
+      $json->username,
+      $json->about,
+      $json->password
+    );
+  }
+
+  private function serializeUser($user) {
     $responseBody = [
-      'id' => $createdUser->getId(),
-      'username' => $createdUser->getUsername(),
-      'about' => $createdUser->getAbout()
+      'id' => $user->getId(),
+      'username' => $user->getUsername(),
+      'about' => $user->getAbout()
     ];
     return $this->json($responseBody);
   }
