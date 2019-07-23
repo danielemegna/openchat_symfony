26768954a26eae56f786a7780d16a8ee1c4c91f0
Daniele Megna <daniele.megna@xpeppers.com>
2019-07-23 10:16:04 +0200

    Remove unused use import

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -10,7 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class UsersController extends Controller {
 
